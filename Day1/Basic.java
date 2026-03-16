b2c4dddd7f8b22731553ac0bb9d2e39cbf2b6b13
sree-soumya2812 <sreesoumyagollapinni@gmail.com>
2026-03-16 10:18:27 +0530

    Add Basic class with main method and max calculation

---
+++ Day1/Basic.java
@@ -0,0 +1,27 @@
+ublic class Basic {
+//	int num=200;
+//	void display()
+//	{
+//		System.out.println("Display");
+//	}
+	int n1=5;
+
+	public static void main(String[] args) {
+//		Basic ob=new Basic();
+//		System.out.println(ob.num);
+//		ob.display();
+//		Basic ob2=new Basic();
+//		ob2.num=400;
+//		System.out.println(ob2.num);
+//		ob2.display();
+//		// TODO Auto-generated method stub
+//		System.out.println(-3>>2);
+		Basic ob=new Basic();
+//		System.out.println((5>3)?5:3);
+		int max=((5>8)?5:(4>8)?4:(8>3)?8:3);
+		System.out.println(max);
+		
+
+	}
+
+}
